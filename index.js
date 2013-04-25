4d58da2817d39d7b14b37277e5821b7cb9c4b279
Ilya Vassilevsky <vassilevsky@gmail.com>
2013-04-25 18:27:27 +0400

    v1

---
+++ index.js
@@ -0,0 +1,45 @@
+var map = new YMaps.Map(document.getElementById('map_placeholder'));
+var plotted_venues_ids = {};
+
+YMaps.Events.observe(map, map.Events.BoundsChange, function(map) {
+  var bounds = map.getBounds();
+
+  jQuery.ajax({
+    url: 'https://api.foursquare.com/v2/venues/search',
+    dataType: 'jsonp',
+    data: {
+      client_id: 'XKBFBXHOCLKHTLGERTD2FR0XEZM0A0RW5MSWSCNGNFW10TVM',
+      client_secret: 'IWD13I0PNJLXMJVNROPY5VYRD05HY2B1OXLL0P5VLC5TIFMW',
+      v: 20111030,
+      intent: 'browse',
+      sw: ''+bounds.getBottom()+','+bounds.getLeft(),
+      ne: ''+bounds.getTop()+','+bounds.getRight(),
+      limit: 50,
+      locale: 'ru'
+    },
+    cache: true,
+    success: function(data) {
+      if (data.response.venues) {
+        for(var i = 0; i < data.response.venues.length; i++) {
+          var venue = data.response.venues[i];
+
+          if (plotted_venues_ids[venue.id]) {
+            var venue_location = new YMaps.GeoPoint(venue.location.lng, venue.location.lat);
+            var venue_name = venue.name;
+
+            if (venue.hereNow.count) {
+              venue_name += ' (' + venue.hereNow.count + ')';
+            }
+
+            var placemark = new YMaps.Placemark(venue_location);
+            placemark.setIconContent(venue_name);
+            map.addOverlay(placemark);
+            plotted_venues_ids[venue.id] = true;
+          }
+        }
+      }
+    }
+  });
+});
+
+map.setCenter(new YMaps.GeoPoint(48.397473, 54.318478), 17, YMaps.MapType.SATELLITE);
